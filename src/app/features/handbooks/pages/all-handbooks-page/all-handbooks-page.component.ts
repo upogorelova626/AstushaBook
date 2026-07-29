421c3f0877894e73a0d2270a65c5c259bbce1d1d
Julia <upogorelova626@gmail.com>
2026-07-29 22:11:38 +0300

    feat: add handbook list pagination with intersection observer

--- src/app/features/handbooks/pages/all-handbooks-page/all-handbooks-page.component.ts
+++ src/app/features/handbooks/pages/all-handbooks-page/all-handbooks-page.component.ts
@@ -1,8 +1,12 @@
 import {
+    AfterViewInit,
     ChangeDetectionStrategy,
     Component,
+    ElementRef,
     inject,
-    OnInit
+    OnDestroy,
+    OnInit,
+    ViewChild
 } from '@angular/core';
 import {HandbooksFiltersComponent} from '../../components/all-handbooks-components/handbooks-filters/handbooks-filters.component';
 import {HandbookToolbarComponent} from '../../components/all-handbooks-components/handbook-toolbar/handbook-toolbar.component';
@@ -24,10 +28,32 @@ import {HandbooksListService} from '../../services/handbooks-list.service';
     styleUrl: './all-handbooks-page.component.less',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AllHandbooksPageComponent implements OnInit {
+export class AllHandbooksPageComponent
+    implements OnInit, OnDestroy, AfterViewInit
+{
+    @ViewChild('loadMore')
+    private loadMore!: ElementRef<HTMLElement>;
+    private observer!: IntersectionObserver;
+
     private readonly handbooksListService = inject(HandbooksListService);
 
     ngOnInit(): void {
         this.handbooksListService.load();
+    }
+
+    ngAfterViewInit() {
+        this.observer = new IntersectionObserver(entries => {
+            const trigger = entries[0];
+
+            if (trigger.isIntersecting) {
+                this.handbooksListService.loadMore();
+            }
+        });
+
+        this.observer.observe(this.loadMore.nativeElement);
+    }
+
+    ngOnDestroy() {
+        this.observer.disconnect();
     }
 }
